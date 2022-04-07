5599b6bbce69c29a1ae6c3d21f079fc44d55cc91
seagull <amysxt2021@163.com>
2022-04-07 15:06:06 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,32 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    /* your code here 
+    no cycle, return 0, otherwise return 1*/
+    if (head == NULL) {
+        return 0;
+    }
+
+    node *tortoise = head;
+    node *hare = head;
+
+    while (1) {
+    hare = hare -> next;   
+    if (hare == NULL) {
+        return 0;
+    }
+    hare = hare -> next;
+    if (hare == NULL) {
+        return 0;
+    }
+
+    tortoise = tortoise -> next;
+    if (tortoise == NULL) {
+        return 0;
+    }
+    if (hare == tortoise) {
+        return 1;
+        }
+    }
     return 0;
 }
